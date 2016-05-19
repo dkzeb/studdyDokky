91b9715f1ccdad30a5e8fa51ba35b45ff6945c54
zeb <sthazeb@gmail.com>
2016-05-19 23:54:49 +0200

    Okay, gik amok, har implementeret IndoorAtlas, og menu - vi er on track... vi snakker om resten imorgen ;)

--- app/src/main/java/dk/teamawesome/studdydokky/StuddyDokkyMap.java
+++ app/src/main/java/dk/teamawesome/studdydokky/StuddyDokkyMap.java
@@ -1,16 +1,117 @@
 package dk.teamawesome.studdydokky;
 
+import android.Manifest;
+import android.support.v7.app.ActionBar;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.graphics.PointF;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 public class StuddyDokkyMap extends AppCompatActivity {
+
+    private static final String TAG = "IndoorAtlasExample";
+
+    // blue dot radius in meters
+    private static final float dotRadius = 1.0f;
+
+    private IALocationManager mIALocationManager;
+    private IAResourceManager mFloorPlanManager;
+    private IATask<IAFloorPlan> mPendingAsyncResult;
+    private IAFloorPlan mFloorPlan;
+    private BlueDotView mImageView;
+    private long mDownloadId;
+    private DownloadManager mDownloadManager;
+
+
+    private IALocationListener mLocationListener = new IALocationListenerSupport() {
+        @Override
+        public void onLocationChanged(IALocation location) {
+            Log.d(TAG, "location is: " + location.getLatitude() + "," + location.getLongitude());
+            if (mImageView != null && mImageView.isReady()) {
+                IALatLng latLng = new IALatLng(location.getLatitude(), location.getLongitude());
+                PointF point = mFloorPlan.coordinateToPoint(latLng);
+                mImageView.setDotCenter(point);
+                mImageView.postInvalidate();
+            }
+        }
+    };
+
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+
+        @Override
+        public void onEnterRegion(IARegion region) {
+            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+                String id = region.getId();
+                Log.d(TAG, "floorPlan changed to " + id);
+                Toast.makeText(StuddyDokkyMap.this, id, Toast.LENGTH_SHORT).show();
+                fetchFloorPlan(id);
+            }
+        }
+
+        @Override
+        public void onExitRegion(IARegion region) {
+            // leaving a previously entered region
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.studdy_dokky_map);
+
+        String[] neededPermissions = {
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+
+
+        // logo
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+            actionBar.setTitle("studieDOKK1");
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setLogo(R.drawable.studiedokk1_logo_transparent_white);
+        }
+        // Menu - slider fra siden :D whey
 
         SlidingMenu menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT);
@@ -22,5 +123,151 @@ public class StuddyDokkyMap extends AppCompatActivity {
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.menu);
 
+        // IndoorAtlas
+        findViewById(android.R.id.content).setKeepScreenOn(true);
+
+        mImageView = (BlueDotView) findViewById(R.id.floorPlanImage);
+
+        mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        mIALocationManager = IALocationManager.create(this);
+        mFloorPlanManager = IAResourceManager.create(this);
+
+        /* optional setup of floor plan id
+           if setLocation is not called, then location manager tries to find
+           location automatically */
+        final String floorPlanId = getString(R.string.floorplan_id);
+        if (!TextUtils.isEmpty(floorPlanId)) {
+            final IALocation location = IALocation.from(IARegion.floorPlan(floorPlanId));
+            mIALocationManager.setLocation(location);
+        }
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mIALocationManager.destroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // starts receiving location updates
+        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mLocationListener);
+        mIALocationManager.registerRegionListener(mRegionListener);
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIALocationManager.removeLocationUpdates(mLocationListener);
+        mIALocationManager.unregisterRegionListener(mRegionListener);
+        unregisterReceiver(onComplete);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Handle if any of the permissions are denied, in grantResults
+    }
+
+    /**
+     * Methods for fetching floor plan data and bitmap image.
+     * Method {@link #fetchFloorPlan(String id)} fetches floor plan data including URL to bitmap
+     */
+
+     /*  Broadcast receiver for floor plan image download */
+    private BroadcastReceiver onComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
+            if (id != mDownloadId) {
+                Log.w(TAG, "Ignore unrelated download");
+                return;
+            }
+            Log.w(TAG, "Image download completed");
+            Bundle extras = intent.getExtras();
+            DownloadManager.Query q = new DownloadManager.Query();
+            q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
+            Cursor c = mDownloadManager.query(q);
+
+            if (c.moveToFirst()) {
+                int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    // process download
+                    String filePath = c.getString(c.getColumnIndex(
+                            DownloadManager.COLUMN_LOCAL_FILENAME));
+                    showFloorPlanImage(filePath);
+                }
+            }
+            c.close();
+        }
+    };
+
+    private void showFloorPlanImage(String filePath) {
+        Log.w(TAG, "showFloorPlanImage: " + filePath);
+        mImageView.setRadius(mFloorPlan.getMetersToPixels() * dotRadius);
+        mImageView.setImage(ImageSource.uri(filePath));
+    }
+
+    /**
+     * Fetches floor plan data from IndoorAtlas server. Some room for cleaning up!!
+     */
+    private void fetchFloorPlan(String id) {
+        cancelPendingNetworkCalls();
+        final IATask<IAFloorPlan> asyncResult = mFloorPlanManager.fetchFloorPlanWithId(id);
+        mPendingAsyncResult = asyncResult;
+        if (mPendingAsyncResult != null) {
+            mPendingAsyncResult.setCallback(new IAResultCallback<IAFloorPlan>() {
+                @Override
+                public void onResult(IAResult<IAFloorPlan> result) {
+                    Log.d(TAG, "fetch floor plan result:" + result);
+                    if (result.isSuccess() && result.getResult() != null) {
+                        mFloorPlan = result.getResult();
+                        String fileName = mFloorPlan.getId() + ".img";
+                        String filePath = Environment.getExternalStorageDirectory() + "/"
+                                + Environment.DIRECTORY_DOWNLOADS + "/" + fileName;
+                        File file = new File(filePath);
+                        if (!file.exists()) {
+                            DownloadManager.Request request =
+                                    new DownloadManager.Request(Uri.parse(mFloorPlan.getUrl()));
+                            request.setDescription("IndoorAtlas floor plan");
+                            request.setTitle("Floor plan");
+                            // requires android 3.2 or later to compile
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.
+                                        Request.VISIBILITY_HIDDEN);
+                            }
+                            request.setDestinationInExternalPublicDir(Environment.
+                                    DIRECTORY_DOWNLOADS, fileName);
+
+                            mDownloadId = mDownloadManager.enqueue(request);
+                        } else {
+                            showFloorPlanImage(filePath);
+                        }
+                    } else {
+                        // do something with error
+                        if (!asyncResult.isCancelled()) {
+                            Toast.makeText(StuddyDokkyMap.this,
+                                    (result.getError() != null
+                                            ? "error loading floor plan: " + result.getError()
+                                            : "access to floor plan denied"), Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                }
+            }, Looper.getMainLooper()); // deliver callbacks in main thread
+        }
+    }
+
+    private void cancelPendingNetworkCalls() {
+        if (mPendingAsyncResult != null && !mPendingAsyncResult.isCancelled()) {
+            mPendingAsyncResult.cancel();
+        }
     }
 }

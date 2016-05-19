8b9bb2f26aaf406493375eea25e828dc95f26132
zeb <sthazeb@gmail.com>
2016-05-19 19:35:48 +0200

    Sliding Menu med cat's

--- app/src/main/java/dk/teamawesome/studdydokky/StuddyDokkyMap.java
+++ app/src/main/java/dk/teamawesome/studdydokky/StuddyDokkyMap.java
@@ -3,11 +3,24 @@ package dk.teamawesome.studdydokky;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
 public class StuddyDokkyMap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.studdy_dokky_map);
+
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
+
     }
 }

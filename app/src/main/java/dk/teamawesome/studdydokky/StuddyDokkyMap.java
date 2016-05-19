57195ad2da3c097bf04f8eb6a0bafcfc03d41d65
zeb <sthazeb@gmail.com>
2016-05-19 16:12:20 +0200

    SlidingMenu og Main Activity

---
+++ app/src/main/java/dk/teamawesome/studdydokky/StuddyDokkyMap.java
@@ -0,0 +1,13 @@
+package dk.teamawesome.studdydokky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StuddyDokkyMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.studdy_dokky_map);
+    }
+}

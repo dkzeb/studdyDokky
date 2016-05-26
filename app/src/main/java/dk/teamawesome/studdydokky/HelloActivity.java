6d63286cb9f108ad627aab58de9ad918d570bab0
zeb <sthazeb@gmail.com>
2016-05-26 11:55:43 +0200

    Master Race - animation

--- app/src/main/java/dk/teamawesome/studdydokky/HelloActivity.java
+++ app/src/main/java/dk/teamawesome/studdydokky/HelloActivity.java
@@ -1,10 +1,15 @@
 package dk.teamawesome.studdydokky;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -14,6 +19,7 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         prefs = getApplicationContext().getSharedPreferences(getString(R.string.prefs_name), Context.MODE_PRIVATE);
         boolean firstRun = prefs.getBoolean("firstRun", true); // vi går ud fra at det her er first run
         if(!firstRun){
@@ -22,8 +28,19 @@ public class HelloActivity extends AppCompatActivity {
             finish();
         } else {
             setContentView(R.layout.activity_hello);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.hide();
 
-
+            ImageButton nextBtn = (ImageButton) findViewById(R.id.welcome_next_btn);
+            if(nextBtn != null){
+                nextBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent activityIntent = new Intent(getApplicationContext(), ChooseActivities.class);
+                        startActivity(activityIntent);
+                    }
+                });
+            }
         }
     }
 }

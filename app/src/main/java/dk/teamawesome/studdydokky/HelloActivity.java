8b4937aa58ecd63b360b6eaf0d1b8d9cc76bdbf8
zeb <sthazeb@gmail.com>
2016-05-26 10:24:28 +0200

    Cleanup - isle 5

--- app/src/main/java/dk/teamawesome/studdydokky/HelloActivity.java
+++ app/src/main/java/dk/teamawesome/studdydokky/HelloActivity.java
@@ -1,13 +1,29 @@
 package dk.teamawesome.studdydokky;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HelloActivity extends AppCompatActivity {
 
+    private SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+
+        prefs = getApplicationContext().getSharedPreferences(getString(R.string.prefs_name), Context.MODE_PRIVATE);
+        boolean firstRun = prefs.getBoolean("firstRun", true); // vi går ud fra at det her er first run
+        if(!firstRun){
+            Intent mainIntent = new Intent(getApplicationContext(), StuddyDokkyMap.class);
+            startActivity(mainIntent);
+            finish();
+        } else {
+            setContentView(R.layout.activity_hello);
+
+
+        }
     }
 }
